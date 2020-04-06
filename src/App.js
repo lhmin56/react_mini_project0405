5d7161bb45944545cdf62dd82268ccf559299576
lhmin <lhmin56@naver.com>
2020-04-06 22:49:50 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a>
-          test test
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+  render(){
+
+    console.log("test")
+    
+    return(
+      <div>  
+        <h1>test</h1>
+      </div>
+      
+    );
+  }
 }
-
-export default App;
